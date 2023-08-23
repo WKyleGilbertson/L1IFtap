2f2d0744a596115754a46303533fbe8a34866595
W. Kyle Gilbertson <wkyleg@gmail.com>
2023-08-23 10:17:21 -0600

    Preparing to add networking

--- L1IFtap.c
+++ L1IFtap.c
@@ -1,6 +1,13 @@
+#ifndef WIN32_LEAN_AND_MEAN
+#define WIN32_LEAN_AND_MEAN
+#endif
+
+#pragma comment(lib, "Ws2_32.lib")
 #pragma comment(lib, "lib/FTD2XX.lib")
 #include "inc/circular_buffer.h"
 #include "inc/ftd2xx.h"
+#include <winsock2.h>
+#include <ws2tcpip.h>
 #include <stdint.h>
 #include <stdio.h>
 
